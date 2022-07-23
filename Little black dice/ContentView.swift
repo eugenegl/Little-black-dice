311d64f94b027fe6b7a02a486f826787eb39c7b1
Eugene Golota <golota.eg@gmail.com>
2022-07-24 01:44:09 +0300

    Initial Commit

---
+++ Little black dice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Little black dice
+//
+//  Created by Голота Евгений on 24.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Little black dice/Little_black_diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_black_diceApp.swift
+//  Little black dice
+//
+//  Created by Голота Евгений on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Little_black_diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
